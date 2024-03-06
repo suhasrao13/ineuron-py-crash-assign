0bc725ed22d402491cac725583f8687b072bb072
Suhas Rao <83769454+suhasrao13@users.noreply.github.com>
2024-03-06 20:19:41 +0530

    chore: add main to interact with user

--- task.py
+++ task.py
@@ -38,4 +38,55 @@ class DatabaseManage(object):
               cur.execute(sql,[id])
               return True
           except Exception:
-            return False        
+            return False    
+
+
+def main():
+  print("*"*40)
+  print("\n::COURSE MANAGEMENT ::\n")
+
+ 
+  db = DatabaseManage()
+
+  print("#"*40)
+  print("\n::user Manual::\n")
+  print('press 1.Insert a new course')
+  print('press 2.show all courses')
+  print('press 3.Delete a course (ned id of course)')
+
+  choice= input("\nEnter your choice:")
+  
+  if choice=='1':
+    name=input("\n Enetr course name :")
+    description=input("\n Enetr description name :")
+    price=input("\nEnter the price :")
+    private=input("\Is this course private(0/1):")
+ 
+    if db.insert_data([name,description,price,private]):
+      print("Course inserted successfully")
+    else:
+      print("OOPS something error")
+    
+  elif choice=='2':
+    print("\n::COURSE list::")
+
+    for index,item in enumerate(db.fetech_dtaa()):
+      print("\n Sl no:" +str(index +1))
+      print("Course ID :"+str(item[0]))
+      print("Course name :"+str(item[1]))
+      print("Course description:"+str(item[2]))
+      print("Course price :"+str(item[3]))
+      private = 'yes' if item[4] else 'NO'
+      print("IS private:"+private)
+      print("\n")
+      #print("Course private :"+str(item[4]))
+
+  elif choice=='3':
+    record_id=int(input("\nEnter the id of the course you want to delete:"))
+    if db.delete_data(record_id):
+      print("Course was deleted successfully")
+    else:
+      print("OOPS something error")
+    
+  else:
+    print("\n BAD CHOICE:")
