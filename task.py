ad49d7bba44d491153fe678a80a1ee409646568d
Suhas Rao <83769454+suhasrao13@users.noreply.github.com>
2024-03-06 20:18:06 +0530

    chore: add, delete, fetech data in DB

--- task.py
+++ task.py
@@ -10,3 +10,32 @@ class DatabaseManage(object):
               cur.execute("create table if not exists course(ID INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,description TEXT,Price TEXT,IS_private BOOLEAN NOT NULL DEFAULT 1)")
             except Exception:
               print("Unable to create DB !")
+
+         def insert_data(self, data):
+          try:
+            with con:
+              cur=con.cursor()
+              cur.execute("INSERT INTO COURSE(name,descrption,price,is_private) VALUES(?,?,?,?)",data)
+            return True
+          except Exception:
+            return False
+      #read data
+        def fetch_adata(self):
+          try:
+            with con:
+              cur=con.cursor()
+              cur.execute('SELECT * FROM COURSES')
+              return cur.fetchall()
+          except Exception:
+            False
+        
+        def delete_data(self,id):
+          try:
+            with con:
+              cur=con.cursor()
+              cur.execute()
+              sql="DELETE FROM course WHERE id=?"
+              cur.execute(sql,[id])
+              return True
+          except Exception:
+            return False        
